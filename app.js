7b16bc8e1470a59b419c4f93431479c951e10bff
lundregan <lundregan.ethan@gmail.com>
2020-08-11 01:28:11 +0100

    Simple chat with sockets and node

---
+++ app.js
@@ -0,0 +1,32 @@
+const app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+})
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('disconnect', () => {
+        console.log('User Disconnected');
+    });
+});
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      console.log('message: ' + msg);
+      io.emit('chat message', msg);
+    });
+});
+
+// io.on('connection', (socket) => {
+//     socket.on('chat message', (msg) => {
+//         io.emit('chat message', msg);
+//     });
+// });
+
+http.listen(3000, () => {
+    console.log('Listening on Port 3000')
+});
